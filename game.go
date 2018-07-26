c4cdae990fd7e4d827b66b9eccbd2e9c0ccfc0f3
ivenk <ivenkoethke@hotmail.de>
2018-07-26 22:09:31 +0200

    Started implementation of the game. Defined API, consists of two methods. Implementation nearly done. Nicely commented

---
+++ game.go
@@ -0,0 +1,94 @@
+package main
+
+//API
+//
+//Usage:
+// newGameState = player.makeMove(game)
+//
+
+
+type ticktacktoe struct {
+	//playing field
+	//field[x] = 0 : free; = 1: marked by player 1; =  2: marked by player 2
+	//treated like
+	//[0,1,2]
+	//[3,4,5]
+	//[6,7,8]
+	field[9] int
+	moveCounter int
+}
+
+// allows a player to make a move by selecting a field on the board.
+func (game ticktacktoe) MakeMove(fieldNumber int) ticktacktoe {
+	// we asume the players do their moves successively
+	game.field[fieldNumber] = (game.moveCounter%2) + 1
+	return game
+}
+
+// Checks the current state of the game.
+// returns
+// 0 : game not finished;
+// 1 : player 1 won;
+// 2 : player 2 won;
+// 3 : no winner but game finished (tie)
+func (game ticktacktoe) CheckState() int {
+	var player = 1
+
+	// check each player individually
+	for i := 0 ; i < 2 ; i++ {
+
+		//check horizontally
+		if game.checkRow(0, player) {
+			return player
+		} else if game.checkRow( 3, player) {
+			return player
+		} else if game.checkRow( 6, player){
+			return player
+		}
+
+		//check vertically
+		if game.checkColumn(0, player) {
+			return player
+		} else if game.checkColumn(1, player) {
+			return player
+		} else if game.checkColumn(2, player) {
+			return player
+		}
+
+		//check cross
+		//TODO Add cross check
+
+		player = 2
+	}
+
+	// are there still free fields ?
+	if contains(game.field, 0) {
+		return 0
+	} else {
+		// no fields left and no winner found. Its a tie !
+		return 3
+	}
+}
+
+//Checks if the entire row is marked with one value
+func (game ticktacktoe) checkRow(rowStartField int, valueToCheck int) bool {
+	return game.field[rowStartField] == valueToCheck &&
+			game.field[rowStartField + 1] == valueToCheck &&
+			game.field[rowStartField + 2] ==valueToCheck
+}
+
+func (game ticktacktoe) checkColumn(columnStartField int, valueToCheck int) bool {
+	return game.field[columnStartField] == valueToCheck &&
+			game.field[columnStartField + 3] == valueToCheck &&
+			game.field[columnStartField + 6] == valueToCheck
+}
+
+// checks whether a value is present in an array
+func contains(array [9]int, value int) bool {
+	for _, i := range array {
+		if array[i] == value {
+			return true
+		}
+	}
+	return false
+}
