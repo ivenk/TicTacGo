0aa0559e2181526daf7fdadc5a7c85569eacb277
ivenk <ivenkoethke@hotmail.de>
2018-08-09 23:14:01 +0200

    Completed test dummy and added first test case.

--- game_test.go
+++ game_test.go
@@ -5,8 +5,10 @@ import "testing"
 
 var checkStateTests = []struct {
 	description string
+	object ticktacktoe
+	expected int
 }{
-	{""},
+	{"Empty playing field", ticktacktoe{[9]int{0,0,0,0,0,0,0,0,0}, 0}, 0},
 }
 
 func TestCheckState(t *testing.T) {
