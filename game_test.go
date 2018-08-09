246c53ae22287c175f5b386a32e24c4676659e88
ivenk <ivenkoethke@hotmail.de>
2018-08-09 23:17:46 +0200

    Added another testcase

--- game_test.go
+++ game_test.go
@@ -9,6 +9,7 @@ var checkStateTests = []struct {
 	expected int
 }{
 	{"Empty playing field", ticktacktoe{[9]int{0,0,0,0,0,0,0,0,0}, 0}, 0},
+	{"Draw", ticktacktoe{[9]int {1, 2 , 1, 2, 2, 1, 1, 1, 2}, 9}, 3},
 }
 
 func TestCheckState(t *testing.T) {
