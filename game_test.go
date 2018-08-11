b8ffd41210248d43b1a42095a8fa2e95ea4ab9e4
ivenk <ivenkoethke@hotmail.de>
2018-08-11 14:43:09 +0200

    Added testcases for a victory for each player.

--- game_test.go
+++ game_test.go
@@ -10,6 +10,9 @@ var checkStateTests = []struct {
 }{
 	{"Empty playing field", ticktacktoe{[9]int{0,0,0,0,0,0,0,0,0}, 0}, 0},
 	{"Draw", ticktacktoe{[9]int {1, 2 , 1, 2, 2, 1, 1, 1, 2}, 9}, 3},
+	{"Victory player one", ticktacktoe{ [9] int {1, 2 , 2, 1 , 0 , 0, 1, 0 , 0}, 5}, 1 },
+	{"Victory player two", ticktacktoe{[9] int {2, 1 , 1, 2 , 0, 0, 2, 0, 0}, 5}, 2 },
+
 }
 
 func TestCheckState(t *testing.T) {
